da531d75a76d8e1c77ee8e249950aa45cd0bac3a
Jabulani00 <jabulanimosesgwala@gmail.com>
2024-10-21 13:51:50 +0200

    spms

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD7u_jf9-wQ_Mth7uy6cRzG97urfJvtRpU",
+    authDomain: "attendify-6202e.firebaseapp.com",
+    projectId: "attendify-6202e",
+    storageBucket: "attendify-6202e.appspot.com",
+    messagingSenderId: "549998560691",
+    appId: "1:549998560691:web:1ade003dd84f944a57b7c6"
+  }
 };
 
 /*
