f414400bfb1a0f60d56c9fb53c27fcc4f4ec6d2a
slindokuhl <slindokuhleatlehang22009757@gmail.com>
2024-10-21 14:15:29 +0200

    login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD7u_jf9-wQ_Mth7uy6cRzG97urfJvtRpU",
+    authDomain: "attendify-6202e.firebaseapp.com",
+    projectId: "attendify-6202e",
+    storageBucket: "attendify-6202e.appspot.com",
+    messagingSenderId: "549998560691",
+    appId: "1:549998560691:web:1ade003dd84f944a57b7c6"
+  }
 };
