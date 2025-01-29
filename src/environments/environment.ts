6600592a56bab0e63bc6851fa2dacfc3be7a460d
Nkosi2000 <nkosiphila901@gmail.com>
2025-01-29 11:02:07 +0200

    ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   emailjsPublicKey: 'mnYtu2o7JhKqb73XH',
-  firebaseConfig : {
-    apiKey: "AIzaSyDVQp5oQSJsD_0gQ88IdbRAiF733-bvaWU",
-    authDomain: "scholarship-832f3.firebaseapp.com",
-    projectId: "scholarship-832f3",
-    storageBucket: "scholarship-832f3.firebasestorage.app",
-    messagingSenderId: "1024819620337",
-    appId: "1:1024819620337:web:2cacde86cdabfe8d9cbc50"
+  firebaseConfig :{
+    apiKey: "AIzaSyD7u_jf9-wQ_Mth7uy6cRzG97urfJvtRpU",
+    authDomain: "attendify-6202e.firebaseapp.com",
+    databaseURL: "https://attendify-6202e-default-rtdb.firebaseio.com",
+    projectId: "attendify-6202e",
+    storageBucket: "attendify-6202e.appspot.com",
+    messagingSenderId: "549998560691",
+    appId: "1:549998560691:web:1ade003dd84f944a57b7c6"
   }
 };
 
