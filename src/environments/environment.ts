47e1b8ffce873d80e009230b04b7b6a52841ca03
Nkosi2000 <nkosiphila901@gmail.com>
2025-01-29 08:05:21 +0200

    env to scholarship DB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   emailjsPublicKey: 'mnYtu2o7JhKqb73XH',
   firebaseConfig : {
-    apiKey: "AIzaSyD7u_jf9-wQ_Mth7uy6cRzG97urfJvtRpU",
-    authDomain: "attendify-6202e.firebaseapp.com",
-    projectId: "attendify-6202e",
-    storageBucket: "attendify-6202e.appspot.com",
-    messagingSenderId: "549998560691",
-    appId: "1:549998560691:web:1ade003dd84f944a57b7c6"
+    apiKey: "AIzaSyDVQp5oQSJsD_0gQ88IdbRAiF733-bvaWU",
+    authDomain: "scholarship-832f3.firebaseapp.com",
+    projectId: "scholarship-832f3",
+    storageBucket: "scholarship-832f3.firebasestorage.app",
+    messagingSenderId: "1024819620337",
+    appId: "1:1024819620337:web:2cacde86cdabfe8d9cbc50"
   }
 };
 
